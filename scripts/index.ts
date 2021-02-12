5d2330a1620727564564b7654660175f06ba0cc7
Andrei Šukurov <14295673+bloha@users.noreply.github.com>
2021-02-12 20:29:38 +0200

    fix: add IMessage into exports

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,10 +1,12 @@
 'use strict';
 
+import { IMessage } from './messaging-framework/message/IMessage.js';
 import { controllerFactory } from './messaging-framework/controllerFactory.js';
 import { messageCenterFactory } from './messaging-framework/messageCenterFactory.js';
 
 
 export {
+    IMessage,
     controllerFactory,
     messageCenterFactory
 };
